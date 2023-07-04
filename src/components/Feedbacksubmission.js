fcbcce1434b5657615a0e7110bd951198b916c7a
Sampath7013 <20bq1a4238@vvit.net>
2023-07-04 21:09:03 +0530

    submit commit

--- src/components/Feedbacksubmission.js
+++ src/components/Feedbacksubmission.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Feedbacksubmission() {
   return (
-    <div className='mt-40 ml-64 font-bold text-xl'>
+    <div className='mt-40 ml-64 font-bold text-xl mobile-style'>
         <h1 className='text-green-500'>Your feedback submitted successfully</h1>
     </div>
   )
